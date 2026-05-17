31c58bc87137d8e2953e5f44881eb38a17b99973
Sahil-coder50 <sahilsheoran24@gmail.com>
2026-05-17 10:09:41 +0000

    Added Docker Code

---
+++ {{cookiecutter.project_slug}}/config/celery.py
@@ -0,0 +1,18 @@
+import os
+from celery import Celery
+from celery.signals import setup_logging
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+app = Celery("{{cookiecutter.project_slug}}")
+
+app.config_from_object(
+    "django.conf:settings",
+    namespace="CELERY"
+)
+
+app.autodicover_tasks()
+
+@setup_logging.connect
+def config_loggers(*args, **kwargs):
+    pass
---
+++ {{cookiecutter.project_slug}}/docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+echo "Waiting for postgres..."
+
+while ! nc -z postgres 5432; do
+  sleep 1
+done
+
+echo "PostgreSQL started"
+
+if [ "$RUN_MIGRATIONS" = "1" ]; then
+  echo "Running migrations..."
+  python manage.py migrate --noinput
+  python manage.py collectstatic --noinput
+  python manage.py seed_permissions
+fi
+
+exec "$@"
