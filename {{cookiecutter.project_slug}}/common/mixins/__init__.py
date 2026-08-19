6fc522fc42daffb26e8c7fd9b6d38416c6ec333b
Sahil-coder50 <sahilsheoran24@gmail.com>
2026-08-19 20:07:18 +0530

    fix: Added BaseMixins in init

---
+++ {{cookiecutter.project_slug}}/common/mixins/__init__.py
@@ -0,0 +1,11 @@
+from .audit_mixin import AuditMixin
+from .time_mixin import TimeMixin
+from .soft_delete import SoftDeleteMixin
+from .base_mixin import BaseMixin
+
+__all__ = [
+    "AuditMixin",
+    "TimeMixin",
+    "SoftDeleteMixin",
+    "BaseMixin",
+]
