53ae9b87d5d41490b9f3db141865e61a15024c96
Sahil-coder50 <sahilsheoran24@gmail.com>
2026-08-22 18:22:05 +0530

    Fix default value for is_deleted field in SoftDeleteMixin

--- {{cookiecutter.project_slug}}/common/mixins/soft_delete.py
+++ {{cookiecutter.project_slug}}/common/mixins/soft_delete.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class SoftDeleteMixin(models.Model):
 
-    is_deleted = models.BooleanField(is_deleted=False)
+    is_deleted = models.BooleanField(default=False)
     deleted_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
