66936d848c3078dbc0e51519c17ccb1826216227
Sahil-coder50 <sahilsheoran24@gmail.com>
2026-08-22 11:12:16 +0530

    Add related_name to created_by and updated_by fields

--- {{cookiecutter.project_slug}}/common/mixins/audit_mixin.py
+++ {{cookiecutter.project_slug}}/common/mixins/audit_mixin.py
@@ -5,12 +5,14 @@ class AuditMixin(models.Model):
     created_by = models.ForeignKey(
         "User",
         on_delete=models.SET_NULL,
+        related_name="+",
         blank=True, null=True
     )
 
     updated_by = models.ForeignKey(
         "User",
         on_delete=models.SET_NULL,
+        related_name="+",
         blank=True, null=True
     )
 
